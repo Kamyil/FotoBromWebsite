10d850eafc5fd976a1c43898fefbc104918a5803
Kamyil <plkamil1998@gmail.com>
2018-01-16 17:55:44 +0100

    First commit to Git. Deleted "profil kierowcy"

---
+++ src/parallax.js
@@ -0,0 +1,19 @@
+$(window).scroll(function() {
+var scrollTop = $(this).scrollTop();
+        $('.Photo1').css('top', -(scrollTop * 1) + 'px');
+      
+        
+      
+   
+   
+
+       
+    
+ 
+      
+
+
+
+
+
+});
